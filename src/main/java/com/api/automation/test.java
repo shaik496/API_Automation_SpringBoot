556ec799dc7924dca63e62037ed475a08f62adb8
Shaik Meeravali <44121209+shaik496@users.noreply.github.com>
2024-07-08 00:24:21 +0530

    Removing unwanted files
    test.class

--- src/main/java/com/api/automation/test.java
+++
@@ -1,7 +0,0 @@
-package com.api.automation;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("This is shaik Meeravali");              
-    }
-}
--- src/test/java/com/api/automation/demo/DemoApplicationTests.java
+++ src/test/java/com/api/automation/demo/DemoApplicationTests.java
@@ -8,6 +8,7 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("This is typical SOP statement");
 	}
 
 }
